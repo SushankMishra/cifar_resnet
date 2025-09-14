a51a07117816588862a22d2d16cee9e5ddb9f8a2
SushankMishra <sushankiitd@gmail.com>
2025-09-14 21:24:15 +0530

    Chanfes

--- cifar_resnet/__init__.py
+++ cifar_resnet/__init__.py
@@ -1,9 +1,9 @@
-from .models.resnet_cifar import get_resnet18_cifar10
+from .models.resnet_cifar import get_custom_resnet
 from .data.datasets import CIFAR10Alb, get_transforms
 from .utils import train_utils, lr_utils
 
 __all__ = [
-    "get_resnet18_cifar10",
+    "get_custom_resnet",
     "CIFAR10Alb",
     "get_transforms",
     "train_utils",
